429e37208d423fd8dd5a0f328a977a0095be4759
John Imrie <jgni@cpan.org>
2014-12-29 14:54:48 +0000

    Import of JGNI/Locale-CLDR-Locales-Bg.pm-v0.26.6.tar.gz from CPAN.
    gitpan-cpan-distribution: Locale-CLDR-Locales-Bg
    gitpan-cpan-version:      v0.26.6
    gitpan-cpan-path:         JGNI/Locale-CLDR-Locales-Bg.pm-v0.26.6.tar.gz
    gitpan-cpan-author:       JGNI
    gitpan-cpan-maturity:     released

---
+++ lib/Locale/CLDR/Locales/Bg/Any.pm
@@ -0,0 +1,19 @@
+package Locale::CLDR::Locales::Bg::Any;
+
+# This file auto generated
+#	on Mon 29 Dec 10:25:25 am GMT
+
+use version;
+
+our $VERSION = version->declare('v0.26.6');
+
+use v5.10;
+use mro 'c3';
+use if $^V ge v5.12.0, feature => 'unicode_strings';
+
+use Moose;
+
+extends('Locale::CLDR::Locales::Bg');
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
---
+++ lib/Locale/CLDR/Locales/Bg/Any/Bg.pm
@@ -0,0 +1,23 @@
+package Locale::CLDR::Locales::Bg::Any::Bg;
+# This file auto generated from Data\common\main\bg_BG.xml
+#	on Mon 29 Dec 10:25:25 am GMT
+# XML file generated 2014-07-23 16:10:33 -0500 (Wed, 23 Jul 2014)
+
+use version;
+
+our $VERSION = version->declare('v0.26.6');
+
+use v5.10;
+use mro 'c3';
+use utf8;
+use if $^V ge v5.12.0, feature => 'unicode_strings';
+
+use Moose;
+
+extends('Locale::CLDR::Locales::Bg::Any');
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+# vim: tabstop=4
